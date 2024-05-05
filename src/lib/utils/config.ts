0d62a3565d1add8e8dfbfe97dbba1c2c6b1aa1a6
Arpit Patidar <arpit.ptdr@hotmail.com>
2024-05-05 10:06:52 +0530

    chore: log vars in console

--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -1,6 +1,11 @@
 'use server'
 
 export async function getDeployURl() {
+  console.log(process.env.URL)
+  console.log(process.env.DEPLOY_URL)
+  console.log(process.env.NETLIFY)
+  console.log(process.env.CONTEXT)
+
   return process.env.NETLIFY
     ? process.env.CONTEXT === 'production'
       ? process.env.URL

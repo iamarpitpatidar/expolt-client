a47b28fff46d971109be90f27144ad00c0d815ef
Arpit Patidar <arpit.ptdr@hotmail.com>
2024-05-05 10:17:19 +0530

    chore: log env vas

--- src/lib/utils/config.ts
+++ src/lib/utils/config.ts
@@ -4,10 +4,10 @@ export async function getDeployURl() {
   console.log(process.env.URL)
   console.log(process.env.DEPLOY_URL)
   console.log(process.env.NETLIFY)
-  console.log(process.env.CONTEXT)
+  console.log(process.env.DEPLOY_PRIME_URL)
 
   return process.env.NETLIFY
-    ? process.env.CONTEXT === 'production'
+    ? process.env.NODE_ENV === 'production'
       ? process.env.URL
       : process.env.DEPLOY_URL
     : process.env.DEPLOY_URL

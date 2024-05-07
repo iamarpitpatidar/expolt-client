191a4ea5e01edb88c9a908fe2f1290e6b2cb3ad7
Arpit Patidar <arpit.ptdr@hotmail.com>
2024-05-07 15:24:46 +0530

    chore: added json header

--- src/auth.ts
+++ src/auth.ts
@@ -24,7 +24,12 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       console.log('fetching user', appUrl)
       const response = await fetch(
         `${appUrl}/api/auth/users/me?token=${token.sub}`,
-        { headers: { Accept: 'application/json' } },
+        {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+        },
       ).then((res) => res.json())
       session.user = {
         ...session.user,

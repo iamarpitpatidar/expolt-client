b642d6a6cd82388062e622f941c5907c0c974850
Arpit Patidar <arpit.ptdr@hotmail.com>
2024-05-07 14:59:23 +0530

    chore: added deploy url

--- src/lib/utils/misc.ts
+++ src/lib/utils/misc.ts
@@ -14,7 +14,11 @@ export function getRandomColorHex() {
 }
 
 export async function getDeployURl() {
+  console.log(process.env.NODE_ENV)
+  console.log(process.env.NODE_ENV === 'production')
+  console.log(process.env.URL)
+  console.log(process.env.APP_URL)
   return process.env.NODE_ENV === 'production'
-      ? process.env.URL
-      : process.env.APP_URL
+    ? process.env.URL
+    : process.env.APP_URL
 }

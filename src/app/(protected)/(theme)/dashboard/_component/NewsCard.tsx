129c36c83b9447d512098f652905d7ecbe7ed2da
Arpit Patidar <arpit.ptdr@hotmail.com>
2024-07-02 08:56:41 +0530

    chore: minor changes

--- src/app/(protected)/(theme)/dashboard/_component/NewsCard.tsx
+++ src/app/(protected)/(theme)/dashboard/_component/NewsCard.tsx
@@ -1,11 +1,19 @@
+import { Fragment } from 'react'
+import Link from 'next/link'
 import {
   Card,
   CardContent,
   CardDescription,
   CardTitle,
 } from '@components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@components/ui/carousel'
 import { Badge } from '@components/ui/badge'
-import Link from 'next/link'
 import { articles, lastUpdate } from 'src/database'
 
 async function NewsCard() {
@@ -13,41 +21,57 @@ async function NewsCard() {
   if (
     !articles.chain().data().length ||
     !lastUpdateTime ||
-    lastUpdateTime > Date.now() - 1000 * 60 * 60
+    lastUpdateTime > Date.now() - 1000 * 60 * 60 * 24
   ) {
     const response = await fetch(
-      `https://gnews.io/api/v4/search?q=tech&lang=en&country=in&max=10&apikey=${process.env.NEWS_API_KEY}`,
+      `https://gnews.io/api/v4/search?q=tech&lang=en&country=in&max=5&apikey=${process.env.NEWS_API_KEY}`,
     ).then((res) => res.json())
 
     const { articles: data } = response
+    console.log(response)
     if (!data) return null
     articles.insert(data)
     if (lastUpdateTime) lastUpdate.remove(lastUpdateTime)
     lastUpdate.insert({ name: 'articles', lastUpdate: Date.now() })
   }
-  const article = articles.chain().simplesort('$loki', true).data()[0]
+  const articleResponse = articles.chain().simplesort('$loki', true).data()
 
   return (
-    <Card
-      className="relative overlay"
-      style={{ background: `url(${article.image})`, backgroundSize: 'cover' }}
-    >
-      <CardContent className="p-8 dashboard-hero">
-        <div className="absolute bottom-0 z-20">
-          <Link href={article.url} target="_blank">
-            <Badge className="my-4" variant="secondary">
-              Explore
-            </Badge>
-          </Link>
-          <CardTitle className="text-white text-2xl my-4">
-            {article.title}
-          </CardTitle>
-          <CardDescription className="text-white text-sm mb-8">
-            {article.description}
-          </CardDescription>
-        </div>
-      </CardContent>
-    </Card>
+    <Fragment>
+      <Carousel>
+        <CarouselContent>
+          {articleResponse.map((article, index) => (
+            <CarouselItem key={index}>
+              <Card
+                className="relative overlay"
+                style={{
+                  background: `url(${article.image})`,
+                  backgroundSize: 'cover',
+                }}
+              >
+                <CardContent className="p-8 dashboard-hero">
+                  <div className="absolute bottom-0 z-20">
+                    <Link href={article.url} target="_blank">
+                      <Badge className="my-4" variant="secondary">
+                        Explore
+                      </Badge>
+                    </Link>
+                    <CardTitle className="text-white text-2xl my-4">
+                      {article.title}
+                    </CardTitle>
+                    <CardDescription className="text-white text-sm mb-8">
+                      {article.description}
+                    </CardDescription>
+                  </div>
+                </CardContent>
+              </Card>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </Fragment>
   )
 }
 

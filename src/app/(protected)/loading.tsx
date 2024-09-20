c1cb33bb90de992caff1fffcf2c13bdb19799ebf
Arpit Patidar <arpit.ptdr@hotmail.com>
2024-09-20 06:17:32 +0530

    chore: updated lodaer

--- src/app/(protected)/loading.tsx
+++ src/app/(protected)/loading.tsx
@@ -3,10 +3,9 @@ import ExpoltLogo from '@assets/images/logo.png'
 
 export default function Loading() {
   return (
-    <div>
-      <div className="center-content">
-        <Image src={ExpoltLogo} alt="Expolt Logo" />
-        <span>Expolt</span>
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-pulse-slow">
+        <Image src={ExpoltLogo} alt="Expolt Logo" width={100} height={100} />
       </div>
     </div>
   )
